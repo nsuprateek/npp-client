bfde656d953c1146d14a9ba3f1afef9635d41d17
Suprateek Nag <tipunag12@gmail.com>
2020-10-26 00:58:04 +0100

    added auto reconnect

--- client.js
+++ client.js
@@ -1,25 +1,59 @@
 const WebSocket = require('ws')
-const url = 'ws://localhost:8080'
-const connection = new WebSocket(url)
-
-// const readline = require('readline');
-// readline.emitKeypressEvents(process.stdin);
-// process.stdin.setRawMode(true);
-
 const iohook = require('iohook');
- 
-connection.onopen = () => {
-	iohook.on('keypress', (key) => {
-		connection.send(JSON.stringify(key));
-	});
-	iohook.start();
-};
- 
-connection.onerror = (error) => {
-	console.log(`WebSocket error: ${error}`)
-};
- 
-connection.onmessage = (e) => {
-	let key = JSON.parse(e.data);
-	console.log(key.rawcode);
-};
+var ks = require('node-key-sender');
+
+const PORT = process.env.PORT || 8080;
+const url = 'ws://npp-app.herokuapp.com:80/ws'
+
+var reconnect_time = 0.01 //sec
+
+function connect() {
+
+	const connection = new WebSocket(url);
+	 
+	connection.onopen = () => {
+		console.log("Connection started");
+
+		iohook.on('keypress', (key) => {
+			connection.send(JSON.stringify(key));
+		});
+		iohook.start();
+	};
+
+	connection.onclose = (e) => {
+		console.log(`Socket is closed. Attempting to reconnect in ${reconnect_time} second...`, e.reason);
+		setTimeout(function() {
+	        if (reconnect_time <= 5*60*1000) { // 5 minutes
+	        	connect();
+	        	reconnect_time *= 1.25;
+	    		console.clear();
+	        }
+	        else {
+	        	connection.close();
+	        }
+
+	    }, reconnect_time * 1000);
+	};
+	 
+	connection.onerror = (error) => {
+		err = JSON.stringify(error);
+		console.log(`WebSocket error: ${err}`)
+	};
+
+	connection.onmessage = (e) => {
+		let key = JSON.parse(e.data);
+		console.log(key.rawcode);
+
+		if (key.rawcode === 65363) {
+			ks.sendKey('a');
+
+		// if ()
+		// 	ks.sendKey('left');
+		// 	ks.sendKey('right');
+		// 	ks.sendKey('up');
+		// 	ks.sendKey('q');
+		}
+	};
+}
+
+connect();
